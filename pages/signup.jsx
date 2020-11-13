92c207f374fce258fcddd497ff3ba3cf01734324
Rafi Azman <7603038+rafiazman@users.noreply.github.com>
2020-11-13 16:07:03 +1300

    Update signup page to use react-sanctum

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,147 +1,200 @@
 /** @format */
 
 import React from 'react';
-import { AuthContext } from '../contexts/AuthContext';
-import Router from 'next/router';
 import styles from '../css/auth.module.scss';
 import PageLoading from '../components/PageLoading';
 import { DebounceInput } from 'react-debounce-input';
+import { withRouter } from 'next/router';
+import { withSanctum } from 'react-sanctum';
+import axios from '../app/axios';
 
-export default class SignUp extends React.Component {
+class SignUp extends React.Component {
+
   constructor(props) {
     super(props);
-    this.state = { loading: false };
+    this.state = {
+      loading: false,
+      email: "",
+      name: "",
+      password: "",
+      confirmPassword: "",
+      errors: {
+        emailTaken: false,
+        nameTaken: false,
+        passwordMismatch: false,
+      },
+      errorMessage: "",
+    };
+  }
+
+  setLoading(value) {
+    this.setState(() => ({ loading: value }));
+  }
+
+  onChangeEmail(e) {
+    const val = e.target.value;
+    this.setState(() => ({ email: val }));
+  }
+  onChangeName(e) {
+    const val = e.target.value;
+    this.setState(() => ({ name: val }));
+  }
+  onChangePassword(e) {
+    const val = e.target.value;
+    this.setState(() => ({ password: val }));
+  }
+  onChangeConfirmPassword(e) {
+    const val = e.target.value;
+    this.setState(() => ({ confirmPassword: val }));
+  }
+
+  onSubmitSignUp(e) {
+    e.preventDefault();
+
+    const { router } = this.props;
+    const { email, name, password, confirmPassword } = this.state;
+
+    if (password != confirmPassword) {
+      this.setState(s => ({
+        errors: {
+          ...s.errors,
+          passwordMismatch: true
+        }
+      }));
+      return;
+    }
+
+    this.setLoading(true);
+    const requestBody = {
+      name: name,
+      email: email,
+      password: password,
+      password_confirmation: confirmPassword,
+    };
+    axios.post('/register', requestBody)
+      .then(() => router.push('/'))
+      .catch(err => {
+        const errRes = err.response.data;
+
+        if (errRes.errors.name) {
+          this.setState(() => ({ errorMessage: errRes.errors.name[0] }));
+        } else if (errRes.errors.email) {
+          this.setState(() => ({ errorMessage: errRes.errors.email[0] }));
+        } else if (errRes.errors.password) {
+          this.setState(() => ({ errorMessage: errRes.errors.password[0] }));
+        } else if (errRes.message) {
+          this.setState(() => ({ errorMessage: errRes.message }));
+        } else {
+          this.setState(() => ({ errorMessage: 'Could not complete the sign up' }));
+        }
+      })
+      .finally(() => {
+        this.setLoading(false);
+      });
+  }
+
+  componentDidMount() {
+    const { authenticated, router } = this.props;
+
+    if (!authenticated) router.push('/');
   }
 
   render() {
-    const setLoading = isLoading => {
-      this.setState(() => ({ loading: isLoading }));
-    };
+    const { loading, errors, errorMessage } = this.state;
+    const debounceTimeout = 400;
+
     return (
       <>
-        {this.state.loading && <PageLoading message='TripTime is creating an account for you :)' />}
-        <AuthContext.Consumer>
-          {({
-            signup,
-            currentUser,
-            userEmail,
-            handleEmailInput,
-            checkEmailOccupied,
-            emailOccupied,
-            handleUserNameInput,
-            checkNameOccupied,
-            nameOccupied,
-            handleUserPassword,
-            handleUserPasswordConfirm,
-            passwordConfirmed,
-            errorMessage,
-            userPassword,
-            userNameInput,
-            userConfirmedPassword,
-            redirectTo,
-          }) => {
-            if (currentUser) Router.push(redirectTo);
+        {loading && <PageLoading message='TripTime is creating an account for you :)' />}
 
-            return (
-              <div className={styles.regFormContainer}>
-                {errorMessage ? (
-                  <div className='failed'>
-                    <p>Sorry, we failed to create an account for you because:</p>
-                    <p>{errorMessage}</p>
-                    <p>Please try again:</p>
-                  </div>
-                ) : (
-                  <h1 className='form-title'>
-                    <img src='/img/logo.svg' alt='' />
-                    Hey, nice to have you joining us :)
-                  </h1>
-                )}
-                <form
-                  className={styles.regForm}
-                  onSubmit={async e => {
-                    e.preventDefault();
-                    setLoading(true);
-                    await signup();
-                    setLoading(false);
-                  }}
-                >
-                  <label>
-                    Enter your Email Address:
-                    <DebounceInput
-                      type='email'
-                      name='email'
-                      required={true}
-                      debounceTimeout={400}
-                      onChange={event => {
-                        handleEmailInput(event);
-                        checkEmailOccupied(event.target.value);
-                      }}
-                      value={userEmail}
-                    />
-                  </label>
-                  {emailOccupied && (
-                    <div className={styles.invalidAlert}>
-                      Sorry, this email address has been occupied. Try another one?`
-                    </div>
-                  )}
-                  <label>
-                    Enter your Nickname:
-                    <DebounceInput
-                      type='name'
-                      required={true}
-                      minLength={3}
-                      maxLength={14}
-                      name='nickname'
-                      debounceTimeout={400}
-                      onChange={event => {
-                        handleUserNameInput(event);
-                        checkNameOccupied(event.target.value);
-                      }}
-                      value={userNameInput}
-                    />
-                  </label>
-                  {nameOccupied && (
-                    <div className={styles.invalidAlert}>Sorry, this nickname has been occupied. Try another one?</div>
-                  )}
-                  <label>
-                    Create your Password:
-                    <input
-                      type='password'
-                      required={true}
-                      minLength={6}
-                      maxLength={20}
-                      name='password'
-                      onChange={handleUserPassword}
-                      value={userPassword}
-                    />
-                  </label>
-                  <label>
-                    Confirm your Password:
-                    <input
-                      type='password'
-                      required={true}
-                      minLength={6}
-                      maxLength={20}
-                      name='confirm-password'
-                      onChange={handleUserPasswordConfirm}
-                      value={userConfirmedPassword}
-                    />
-                  </label>
-                  {!passwordConfirmed && (
-                    <div className={styles.invalidAlert}>Sorry, the two passwords you entered do not match!</div>
-                  )}
-                  <input
-                    value='Register'
-                    type='submit'
-                    disabled={!passwordConfirmed || emailOccupied || nameOccupied}
-                    className={styles.regSubmit}
-                  />
-                </form>
+        <div className={styles.regFormContainer}>
+          {errorMessage ? (
+            <div className='failed'>
+              <p>Sorry, we failed to create an account for you because:</p>
+              <p>{errorMessage}</p>
+              <p>Please try again:</p>
+            </div>
+          ) : (
+            <h1 className='form-title'>
+              <img src='/img/logo.svg' alt='' />
+              Hey, nice to have you joining us :)
+            </h1>
+          )}
+
+          <form className={styles.regForm}>
+            <label>
+              Enter your Email Address:
+              <DebounceInput
+                type='email'
+                name='email'
+                required={true}
+                debounceTimeout={debounceTimeout}
+                onChange={e => this.onChangeEmail(e)}
+              />
+            </label>
+
+            {errors.emailTaken && (
+              <div className={styles.invalidAlert}>
+                Sorry, this email address has been occupied. Try another one?`
               </div>
-            );
-          }}
-        </AuthContext.Consumer>
+            )}
+
+            <label>
+              Enter your Nickname:
+              <DebounceInput
+                type='name'
+                required={true}
+                minLength={3}
+                maxLength={14}
+                name='nickname'
+                debounceTimeout={debounceTimeout}
+                onChange={e => this.onChangeName(e)}
+              />
+            </label>
+
+            {errors.nameTaken && (
+              <div className={styles.invalidAlert}>Sorry, this nickname has been occupied. Try another one?</div>
+            )}
+
+            <label>
+              Create your Password:
+              <input
+                type='password'
+                required={true}
+                minLength={6}
+                maxLength={20}
+                name='password'
+                onChange={e => this.onChangePassword(e)}
+              />
+            </label>
+            <label>
+              Confirm your Password:
+              <input
+                type='password'
+                required={true}
+                minLength={6}
+                maxLength={20}
+                name='confirm-password'
+                onChange={e => this.onChangeConfirmPassword(e)}
+              />
+            </label>
+
+            {errors.passwordMismatch && (
+              <div className={styles.invalidAlert}>Sorry, the two passwords you entered do not match!</div>
+            )}
+
+            <input
+              value='Register'
+              type='submit'
+              disabled={!Object.values(errors).every(k => k === false)}
+              className={styles.regSubmit}
+              onClick={e => this.onSubmitSignUp(e)}
+            />
+          </form>
+        </div>
       </>
     );
   }
 }
+
+export default withRouter(withSanctum(SignUp));
