d95de36d8e1b8d561a02626d2a68f44b43f4b077
Rafi Azman <7603038+rafiazman@users.noreply.github.com>
2020-11-13 15:53:33 +1300

    Update profile page to use react-sanctum

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -2,27 +2,25 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { AuthContext } from '../contexts/AuthContext';
+import { withSanctum } from 'react-sanctum';
 
-export default class Profile extends React.Component {
+class Profile extends React.Component {
   render() {
+    const { authenticated, user } = this.props;
+
+    if (!authenticated) return <p>You are not logged in. No profile to show.</p>;
+
     return (
-      <AuthContext.Consumer>
-        {({ currentUser }) => {
-          if (currentUser)
-            return (
-              <div style={{ width: '50%', margin: '0 auto' }}>
-                <h1>Profile Page</h1>
-                <Link href={'/timeline'}>
-                  <a>Go to your trip</a>
-                </Link>
-                <p>Welcome to the Profile Page! Here is your profile information:</p>
-                <p>{JSON.stringify(currentUser)}</p>
-              </div>
-            );
-          else return <p>You are not logged in. No profile to show.</p>;
-        }}
-      </AuthContext.Consumer>
+      <div style={{ width: '50%', margin: '0 auto' }}>
+        <h1>Profile Page</h1>
+        <Link href={'/timeline'}>
+          <a>Go to your trip</a>
+        </Link>
+        <p>Welcome to the Profile Page! Here is your profile information:</p>
+        <p>{JSON.stringify(user)}</p>
+      </div>
     );
   }
 }
+
+export default withSanctum(Profile);
